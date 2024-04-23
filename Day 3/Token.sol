991d8f5e3f17fd26f1f54c17104d3dc111318456
Akos-T <akosh.toth@gmail.com>
2024-04-23 16:08:58 +0200

    feat(code): Add comment to smart contracts in Day 3 folder

--- Day 3/Token.sol
+++ Day 3/Token.sol
@@ -1,22 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+// Import 'ERC20', 'AccessControl', '_grantRole()', 'DEFAULT_ADMIN_ROLE', 'onlyRole()', '_mint()'
 import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.6.0/access/AccessControl.sol";
 
 contract Token is ERC20, AccessControl {
+    // Create a role: MINTER_ROLE
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    // Create ERC20 token: Name + Ticker
     constructor() ERC20("Chainlink Bootcamp 2024 Token", "CLBoot24") {
+        // Granting roles to the deployer
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
+    // Mints token to the `to` address with `amount` amount.
+    // Only address with MINTER_ROLE can call this!
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
+    // Overrides ERC20 decimals, our token will have 2 decimals, like fiat currencies
+    // ETH has 18 decimals for example.
+    // Solidity doesn't handle decimal points, that's why we need this.
     function decimals() public pure override returns (uint8) {
-        return 2;
+        return 2; // 100.00
     }
 }
--- Day 3/TokenShop.sol
+++ Day 3/TokenShop.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+// Deploy this contract on Sepolia
+
+// This will allow us to see price and other information
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 interface TokenInterface {
@@ -10,9 +13,11 @@ interface TokenInterface {
 contract TokenShop {
     AggregatorV3Interface internal priceFeed;
     TokenInterface public minter;
-    uint256 public tokenPrice = 200; //1token = 2.00 USD, with 2 decimal places
+    uint256 public tokenPrice = 200; //1 token = 2.00 USD, with 2 decimal places
     address public owner;
 
+    // In this case this address is going to be the previously created token contract's address
+    // We need to defin this when deploying this contract
     constructor(address tokenAddress) {
         minter = TokenInterface(tokenAddress);
 
@@ -31,17 +36,20 @@ contract TokenShop {
      * Returns the latest answer
      */
     function getChainlinkDataFeedLatestAnswer() public view returns (int) {
+        // We're only interested in the price now
         (
             ,
-            /*uint80 roundID*/ int price,
+            /* uint80 roundID */ int price /* uint startedAt */ /* uint timeStamp */ /* uint80 answeredInRound */,
             ,
             ,
 
-        ) = /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/
-            priceFeed.latestRoundData();
+        ) = priceFeed.latestRoundData();
         return price;
     }
 
+    // In: amount of ETH
+    // Out: amount of my token
+    // X ETH = Y MyToken
     function tokenAmount(uint256 amountETH) public view returns (uint256) {
         //Sent amountETH, how many usd I have
         uint256 ethUsd = uint256(getChainlinkDataFeedLatestAnswer()); //with 8 decimal places
@@ -50,9 +58,10 @@ contract TokenShop {
         return amountToken;
     }
 
+    // payable = it can receive tokens, in this case ETH
     receive() external payable {
-        uint256 amountToken = tokenAmount(msg.value);
-        minter.mint(msg.sender, amountToken);
+        uint256 amountToken = tokenAmount(msg.value); // In: amount of ETH, Out: amount of my token
+        minter.mint(msg.sender, amountToken); // Mint MyToken in the amount of `amountToken` to `msg.sender`
     }
 
     modifier onlyOwner() {
@@ -60,7 +69,9 @@ contract TokenShop {
         _;
     }
 
+    // Owner can withdraw the ETH that was sent to the contract in exchange of `MyToken` using the receive function
     function withdraw() external onlyOwner {
         payable(owner).transfer(address(this).balance);
+        // address(this) = address of this contract
     }
 }
